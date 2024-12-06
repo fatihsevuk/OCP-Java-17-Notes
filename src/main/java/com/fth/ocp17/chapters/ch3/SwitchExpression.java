f2defd37ab3ff7dc3fcdfa9a404db38d39468a7e
fatihsevuk <fatihsvk1@gmail.com>
2024-12-06 23:12:41 +0100

    ch3 - switch expressions and previous chapters.

--- src/main/java/com/fth/ocp17/chapters/ch3/SwitchExpression.java
+++ src/main/java/com/fth/ocp17/chapters/ch3/SwitchExpression.java
@@ -56,5 +56,45 @@ public class SwitchExpression {
         printSeason(3); // Winter
         System.out.println();
         printSeason12(4); //SpringSummerFall
+
+
+        int measurement = 10;
+        int size = switch (measurement) {
+            case 5 -> 1;
+            case 10 -> (short) 2;
+            default -> 5;
+            //case 20 -> "3"; // DOES NOT COMPILE
+            //case 40 -> 4L; // DOES NOT COMPILE
+            //case 50 -> null; // DOES NOT COMPILE
+        };
+
+
+        int fish = 5;
+        int length = 12;
+        var name = switch (fish) {
+            case 1 -> "Goldfish";
+            case 2 -> {
+                yield "Trout";
+            }
+            case 3 -> {
+                if (length > 10) yield "Blobfish";
+                else yield "Green";
+            }
+            default -> "Swordfish";
+        };
+
+        getWeather(Season.FALL);
+
+    }
+
+    static String getWeather(Season value) {
+        return switch (value) {
+            case WINTER -> "Cold";
+            case SPRING -> "Rainy";
+            case SUMMER -> "Hot";
+            case FALL -> "Rainy";
+        };
     }
 }
+
+
